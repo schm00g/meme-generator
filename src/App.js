ec9eba1ff4a6134c3d54660a96ceeb3d034fd27d
schm00g <hk8igureu@relay.firefox.com>
2022-01-28 22:49:09 +0000

    init commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import troll from './images/troll-face.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img className="header--image" src={troll} alt="troll"/>
+        <h1 className="header--title">Meme Generator</h1>
+        <h4 className="sub--title">React Course - Project 3</h4>
       </header>
     </div>
   );
