0e7674d1fb54d45a379916253e451710a9e34f17
schm00g <hk8igureu@relay.firefox.com>
2022-01-30 00:36:19 +0000

    object store

--- src/components/Form.js
+++ src/components/Form.js
@@ -3,12 +3,23 @@ import memesData from "../memesData";
 
 
 export default function Form(){
-    const [randomImage, setRandomImage] = React.useState("");
-    function grabRandomImage(event){
+    const [memeImage, setMemeImage] = React.useState({
+        topText: "",
+        bottomText: "",
+        randomImage: "https://i.imgflip.com/30b1gx.jpg"
+    });
+
+    function grabMeme(event){
+        // button default action will submit form & reload page
         event.preventDefault()
         const memesArray = memesData.data.memes;
         const randomNumber = Math.floor(Math.random() * memesArray.length);
-        setRandomImage(memesArray[randomNumber].url);
+        setMemeImage(prevState => {
+            return {
+                ...memeImage,
+                randomImage: memesArray[randomNumber].url
+            }
+        });
     }
 
     return (
@@ -16,9 +27,9 @@ export default function Form(){
             <form className="input--form">
                 <input className="form--input" type="text" placeholder="top text"></input>
                 <input className="form--input" type="text" placeholder="bottom text"></input>
-                <button onClick={grabRandomImage} className="form--button">Get a new image 🖼</button>
+                <button onClick={grabMeme} className="form--button">Get a new image 🖼</button>
             </form>
-            <img src={randomImage} alt="meme" className="meme--image"/>
+            <img src={memeImage.randomImage} alt="meme" className="meme--image"/>
         </main>
     )
 }
